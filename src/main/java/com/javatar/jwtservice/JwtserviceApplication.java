34ee1cc8b7aa16bcaa069747125570da437b70a2
AbdiJavad <Mirjavadabdi@gmail.com>
2024-03-16 00:00:15 +0330

    Updated,Changed

--- src/main/java/com/javatar/jwtservice/JwtserviceApplication.java
+++ src/main/java/com/javatar/jwtservice/JwtserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JwtserviceApplication {
-    
     public static void main(String[] args) {
         SpringApplication.run(JwtserviceApplication.class, args);
     }
